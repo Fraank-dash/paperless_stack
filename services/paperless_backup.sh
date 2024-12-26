6f7dc372e556612953c15bfc90a451e1b476730b
Fraank-dash <61389166+Fraank-dash@users.noreply.github.com>
2024-12-26 13:37:48 +0100

    Update paperless_backup.sh
    additional info added for later

--- services/paperless_backup.sh
+++ services/paperless_backup.sh
@@ -4,9 +4,11 @@
 # set date for filenames, ee.g. create new folder based on date in .../backup/paperless/{date(DDMMYYYY)}_pg_dump .../backup/paperless{date}_files
 
 # db-backup-stuff
+# additional info https://www.postgresql.org/docs/current/libpq-pgpass.html
 # connect to paperless pg_db, pg_dump into file, close connection, 
 # return succeess or fail
 
 # file-backup stuff
+#remote access paperless-dockerstack see https://docs.docker.com/engine/security/protect-access/ ???
 # connect to paperless_webserver, export all files into a zip, close connection
 # return success or fail
