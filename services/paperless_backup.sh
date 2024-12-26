1b0ad86754d1d12f854b294ba56ef95f822da338
Fraank-dash <61389166+Fraank-dash@users.noreply.github.com>
2024-12-26 13:59:02 +0100

    Update paperless_backup.sh
    added some additional info in case of short time memory kicking in...

--- services/paperless_backup.sh
+++ services/paperless_backup.sh
@@ -7,8 +7,28 @@
 # additional info https://www.postgresql.org/docs/current/libpq-pgpass.html
 # connect to paperless pg_db, pg_dump into file, close connection, 
 # return succeess or fail
+pg_dump -h localhost -p 5454 -U paperless paperless > ./paperless_backup/backupfilename
 
 # file-backup stuff
-#remote access paperless-dockerstack see https://docs.docker.com/engine/security/protect-access/ ???
+# remote access paperless-dockerstack see https://docs.docker.com/engine/security/protect-access/ ???
+# see https://docs.paperless-ngx.com/administration/#exporter
 # connect to paperless_webserver, export all files into a zip, close connection
 # return success or fail
+docker compose exec -T webserver document_exporter ../export
+
+
+
+#document_exporter target [-c] [-d] [-f] [-na] [-nt] [-p] [-sm] [-z]
+#optional arguments:
+#-c,  --compare-checksums
+#-d,  --delete
+#-f,  --use-filename-format
+#-na, --no-archive
+#-nt, --no-thumbnail
+#-p,  --use-folder-prefix
+#-sm, --split-manifest
+#-z,  --zip
+#-zn, --zip-name
+#--data-only
+#--no-progress-bar
+#--passphrase
